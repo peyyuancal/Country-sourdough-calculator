0670372c58be8f82cb80690177456d7c83fc3c17
peyyuancal <52394460+peyyuancal@users.noreply.github.com>
2025-11-23 21:31:55 -0800

    Add files via upload

---
+++ sw_sourdough.js
@@ -0,0 +1,36 @@
+const CACHE_NAME = 'country-sourdough-calc-v1';
+const URLS_TO_CACHE = [
+  './',
+  './country_sourdough_calculator_pwa.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(URLS_TO_CACHE))
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.map(key => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      )
+    )
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
